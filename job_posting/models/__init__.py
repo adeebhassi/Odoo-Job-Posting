a4dadda1d567aeea42542aee39c99dfefab6702b
adeebhassi <adeebhassi@gmail.com>
2024-12-02 15:45:27 +0500

    'initial'

---
+++ job_posting/models/__init__.py
@@ -0,0 +1,7 @@
+from . import job_posting
+from . import job_tag
+from . import job_title_tag
+from . import website
+from . import applicant_stage
+from . import job_applicant
+from . import refuse_reason
